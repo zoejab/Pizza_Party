047270b58895665884bb98a0418c8fc0c3115d67
zoejab <zoaton@gmail.com>
2015-11-06 11:48:05 -0500

    setting up heroku

--- app/helpers/yelp_helper.rb
+++ app/helpers/yelp_helper.rb
@@ -1,12 +1,12 @@
-module YelpHelper
-
-client = Yelp::Client.new({ consumer_key: ENV['YELP_API_KEY'],
-                            consumer_secret: ENV['YELP_API_SECRET'],
-                            token: ENV['YELP_API_TOKEN'],
-                            token_secret: ENV['YELP_API_TOKEN_SECRET']
-                          })
-
-    def search
-      Yelp.client.search('San Francisco')
-    end
-end
+# module YelpHelper
+#
+# client = Yelp::Client.new({ consumer_key: ENV['YELP_API_KEY'],
+#                             consumer_secret: ENV['YELP_API_SECRET'],
+#                             token: ENV['YELP_API_TOKEN'],
+#                             token_secret: ENV['YELP_API_TOKEN_SECRET']
+#                           })
+#
+#     def search
+#       Yelp.client.search('San Francisco')
+#     end
+# end
