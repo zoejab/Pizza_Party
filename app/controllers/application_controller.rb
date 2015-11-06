f88c77b31f71e792dbf59455519d4a60d79f9a1d
zoejab <zoaton@gmail.com>
2015-11-06 14:08:37 -0500

    working login and logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  include SessionsHelper
 
+  def authenticate
+  unless logged_in?
+    flash[:error] = "You must be logged in to access this section."
+    redirect_to login_path
+  end
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   has_many :comments
 
   has_secure_password
+
+
+  validates :username, presence: true, length: {maximum: 50}
+
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255}, format: { with: EMAIL_REGEX }, uniqueness: {case_sensitive: false}
+
+  validates :password, presence: true, length: {minimum: 6}
+
 end
