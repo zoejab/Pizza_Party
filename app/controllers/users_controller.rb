97bf6369805b566af0b7fc755dda3da756ae7296
zoejab <zoaton@gmail.com>
2015-11-08 22:18:26 -0500

    finished pizza party

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
 
     if @user.save
-      redirect_to @user
+      redirect_to login_path
     else
       render new_user_path
     end
