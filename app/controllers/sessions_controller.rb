ed6e50cc16d303bcda9b873652bd3b004e0bff62
Zoe Jablow <zoaton@gmail.com>
2015-12-03 17:05:52 -0500

    fixed delete account so only user logged in can see it

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,8 @@ class SessionsController < ApplicationController
   def new
   end
 
+
+
   def create
     user = User.find_by(username: params[:session][:username])
       if user && user.authenticate(params[:session][:password])
